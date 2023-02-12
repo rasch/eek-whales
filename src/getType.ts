b6b322776561169a126b5076fcafa9bffa7b7b5d
Randy Schneck <i@rasch.co>
2023-02-12 14:29:44 -0500

    ✨ Allow getType() to be called without parameters

--- src/getType.ts
+++ src/getType.ts
@@ -1,5 +1,5 @@
 import { isNil } from "./isNil.js"
 
 // getType :: a -> String
-export const getType = (x: any) : string =>
+export const getType = (x?: any) : string =>
   (!isNil(x) && x["@@type"]) || Object.prototype.toString.call(x).slice(8, -1)
