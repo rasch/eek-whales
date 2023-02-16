3e186a970a56a1b16c85760de6e4333d03fa930a
Randy Schneck <i@rasch.co>
2023-02-15 22:44:12 -0500

    🔥 Remove documentation comment from code

--- src/nodeInspect.ts
+++ src/nodeInspect.ts
@@ -1,23 +1,2 @@
 // nodeInspect :: Symbol
-//
-// Using this in the ADTs allows for defining custom `console.log`
-// output for Node.js without the need to `import` the `util` module.
-// This makes it unnecessary to verify a Node.js environment.
-//
-// Example:
-//
-//   import { inspect } from "./inspect.js"
-//   import { nodeInspect } from "./nodeInspect.js"
-//
-//   const Identity = x => ({
-//     ...
-//     },
-//     toString() {
-//       return `Identity(${inspect(x)})`
-//     },
-//     [nodeInspect]() {
-//       return this.toString()
-//     }
-//   })
-//
 export const nodeInspect = Symbol.for("nodejs.util.inspect.custom")
