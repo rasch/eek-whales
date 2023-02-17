c070648444ba98c35291c4f85b4a0ce2804d2692
Randy Schneck <i@rasch.co>
2023-02-17 16:23:03 -0500

    🔥 Remove type signature from src

--- src/getType.ts
+++ src/getType.ts
@@ -4,6 +4,5 @@ interface GetType {
   (a?: any): string
 }
 
-// getType :: a -> String
 export const getType: GetType = a =>
   (!isNil(a) && a["@@type"]) || Object.prototype.toString.call(a).slice(8, -1)
